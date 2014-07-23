d09eb0ef7d2e380cacbcf3c5de43a9cc294d5263
Mads Kristensen <post@madskristensen.net>
2014-07-22 17:24:51 -0700

    Refactorings

--- src/NPM/QuickInfo/NpmQuickInfo.cs
+++ src/NPM/QuickInfo/NpmQuickInfo.cs
@@ -1,59 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web;
+﻿using System.Web;
+using System.Windows;
 using EnvDTE80;
+using JSON_Intellisense._Shared.QuickInfo;
 using Microsoft.JSON.Core.Parser;
-using Microsoft.JSON.Editor;
-using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
 using Newtonsoft.Json.Linq;
 
 namespace JSON_Intellisense.NPM
 {
-    internal class NpmQuickInfo : IQuickInfoSource
+    internal class NpmQuickInfo : QuickInfoSourceBase
     {
-        private ITextBuffer _buffer;
-        private DTE2 _dte;
-
         public NpmQuickInfo(ITextBuffer subjectBuffer, DTE2 dte)
+            : base(subjectBuffer, dte)
+        { }
+
+        public override UIElement Process(string name, JSONParseItem item)
         {
-            _buffer = subjectBuffer;
-            _dte = dte;
-        }
+            NpmPackage package = GetText(name);
 
-        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> qiContent, out ITrackingSpan applicableToSpan)
-        {
-            applicableToSpan = null;
+            if (package == null)
+                return null;
 
-            if (session == null || qiContent == null)
-                return;
-
-            // Map the trigger point down to our buffer.
-            SnapshotPoint? point = session.GetTriggerPoint(_buffer.CurrentSnapshot);
-            if (!point.HasValue)
-                return;
-
-            JSONEditorDocument doc = JSONEditorDocument.FromTextBuffer(_buffer);
-            JSONParseItem item = doc.JSONDocument.ItemBeforePosition(point.Value.Position);
-
-            if (item == null || !item.IsValid)
-                return;
-
-            JSONMember dependency = item.FindType<JSONMember>();
-            if (dependency == null)
-                return;
-
-            var parent = dependency.Parent.FindType<JSONMember>();
-            if (parent == null || !parent.UnquotedNameText.EndsWith("dependencies", StringComparison.OrdinalIgnoreCase))
-                return;
-
-            NpmPackage package = GetText(dependency.UnquotedNameText);
-
-            if (package != null)
-            {
-                applicableToSpan = _buffer.CurrentSnapshot.CreateTrackingSpan(item.Start, item.Length, SpanTrackingMode.EdgeNegative);
-                qiContent.Add(NpmInfoBox.Create(package));
-            }
+            return NpmInfoBox.Create(package);
         }
 
         private NpmPackage GetText(string packageName)
@@ -84,16 +52,6 @@ namespace JSON_Intellisense.NPM
             }
 
             return null;
-        }
-
-        private bool m_isDisposed;
-        public void Dispose()
-        {
-            if (!m_isDisposed)
-            {
-                GC.SuppressFinalize(this);
-                m_isDisposed = true;
-            }
         }
     }
 }
---
+++ src/_Shared/QuickInfo/QuickInfoSourceBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Windows;
+using EnvDTE80;
+using JSON_Intellisense.NPM;
+using Microsoft.JSON.Core.Parser;
+using Microsoft.JSON.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Newtonsoft.Json.Linq;
+
+namespace JSON_Intellisense._Shared.QuickInfo
+{
+    public abstract class QuickInfoSourceBase : IQuickInfoSource
+    {
+        public ITextBuffer _buffer;
+        public DTE2 _dte;
+
+        public QuickInfoSourceBase(ITextBuffer subjectBuffer, DTE2 dte)
+        {
+            _buffer = subjectBuffer;
+            _dte = dte;
+        }
+
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> qiContent, out ITrackingSpan applicableToSpan)
+        {
+            applicableToSpan = null;
+
+            if (session == null || qiContent == null)
+                return;
+
+            // Map the trigger point down to our buffer.
+            SnapshotPoint? point = session.GetTriggerPoint(_buffer.CurrentSnapshot);
+            if (!point.HasValue)
+                return;
+
+            JSONEditorDocument doc = JSONEditorDocument.FromTextBuffer(_buffer);
+            JSONParseItem item = doc.JSONDocument.ItemBeforePosition(point.Value.Position);
+
+            if (item == null || !item.IsValid)
+                return;
+
+            JSONMember dependency = item.FindType<JSONMember>();
+            if (dependency == null)
+                return;
+
+            var parent = dependency.Parent.FindType<JSONMember>();
+            if (parent == null || !parent.UnquotedNameText.EndsWith("dependencies", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            applicableToSpan = _buffer.CurrentSnapshot.CreateTrackingSpan(item.Start, item.Length, SpanTrackingMode.EdgeNegative);
+
+            UIElement element = Process(dependency.UnquotedNameText, item);
+            qiContent.Add(element);
+        }
+
+        public abstract UIElement Process(string name, JSONParseItem item);
+
+        public void Dispose()
+        {
+            // Nothing to dispose
+        }
+    }
+}
