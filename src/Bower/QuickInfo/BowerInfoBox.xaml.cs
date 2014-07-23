d498fc0b530c1c00b6204536755671245aab3292
Mads Kristensen <post@madskristensen.net>
2014-07-22 22:44:02 -0700

    Added VS theme color support. Fixed #1

--- src/Bower/QuickInfo/BowerInfoBox.xaml.cs
+++ src/Bower/QuickInfo/BowerInfoBox.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Shell;
 
 namespace JSON_Intellisense.Bower
 {
@@ -15,7 +17,7 @@ namespace JSON_Intellisense.Bower
         public static BowerInfoBox Create(BowerPackage package)
         {
             _box.lblName.Content = package.Name;
-            _box.lblUrl.Content = package.Url;
+            _box.lblUrl.Content = package.Url.Replace("git://", string.Empty).Replace(".git", string.Empty);
             _box.lblHits.Content = package.Hits.ToString("N0");
 
             return _box;
