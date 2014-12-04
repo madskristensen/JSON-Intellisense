e63cec01ef94f64be167a13fe0cbc7cab92e171b
Mads Kristensen <post@madskristensen.net>
2014-12-04 15:34:08 -0800

    Added QuickInfo for SemVer

--- src/_Shared/QuickInfo/QuickInfoSourceBase.cs
+++ src/_Shared/QuickInfo/QuickInfoSourceBase.cs
@@ -5,6 +5,7 @@ using Microsoft.JSON.Core.Parser;
 using Microsoft.JSON.Editor;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
+using JSON_Intellisense._Shared.Resources;
 
 namespace JSON_Intellisense
 {
@@ -36,7 +37,7 @@ namespace JSON_Intellisense
                 return;
 
             JSONMember dependency = item.FindType<JSONMember>();
-            if (dependency == null || dependency.Name != item)
+            if (dependency == null)
                 return;
 
             var parent = dependency.Parent.FindType<JSONMember>();
@@ -45,10 +46,20 @@ namespace JSON_Intellisense
 
             applicableToSpan = _buffer.CurrentSnapshot.CreateTrackingSpan(item.Start, item.Length, SpanTrackingMode.EdgeNegative);
 
-            UIElement element = CreateTooltip(dependency.UnquotedNameText, item);
+            if (dependency.Value == item)
+            {
+                string version = item.Text.Trim('"', '~', '^');
+                qiContent.Add("   " + version + "  " + Resource.CompletionVersionLatest + Environment.NewLine +
+                              "~" + version + "  " + Resource.CompletionVersionMinor + Environment.NewLine +
+                              "^" + version + "  " + Resource.CompletionVersionMajor);
+            }
+            else
+            {
+                UIElement element = CreateTooltip(dependency.UnquotedNameText, item);
 
-            if (element != null)
-                qiContent.Add(element);
+                if (element != null)
+                    qiContent.Add(element);
+            }
         }
 
         public abstract UIElement CreateTooltip(string name, JSONParseItem item);
