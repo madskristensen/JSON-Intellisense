be952ad5eefc16ddc7dd778f769287bbe9cd5fa0
Sebastian <XmlmXmlmX@users.noreply.github.com>
2016-05-11 15:26:27 +0200

    Added custom feed options

--- src/_Shared/Options.cs
+++ src/_Shared/Options.cs
@@ -11,6 +11,7 @@ namespace JSON_Intellisense
             NpmInstallOnOpen = true;
             BowerInstallOnSave = true;
             NpmInstallOnSave = true;
+            NpmCustomFeedUrl = string.Empty;
         }
 
         [Category("Project open")]
@@ -36,5 +37,17 @@ namespace JSON_Intellisense
         [Description("Automatically calls 'npm install' when package.json is saved.")]
         [DefaultValue(true)]
         public bool NpmInstallOnSave { get; set; }
+        
+        [Category("Custom Feeds")]
+        [DisplayName("Custom NPM Feed Url")]
+        [Description("Enter the URL to your private or custom NPM feed.")]
+        [DefaultValue(true)]
+        public string NpmCustomFeedUrl { get; set; }
+        
+        [Category("Custom Feeds")]
+        [DisplayName("Custom Bower Feed Url")]
+        [Description("Enter the URL to your private or custom Bower feed.")]
+        [DefaultValue(true)]
+        public string BowerCustomFeedUrl { get; set; }
     }
 }
